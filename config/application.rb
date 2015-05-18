6a990236e032dce9b62485eeeb725b1fa1154c3b
Sergio Lima <sergiosouzalima@gmail.com>
2015-05-18 21:37:26 +0000

    Heroku deployment

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,7 @@ Bundler.require(*Rails.groups)
 module Todoapp
   class Application < Rails::Application
     # Use the responders controller from the responders gem
+    #
     config.app_generators.scaffold_controller :responders_controller
 
 
